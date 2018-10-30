27ed23c5b9d93cf3269487b10a39a92c75db8513
dzulwulan <wulantyas9@gmail.com>
2018-10-30 11:21:56 +0700

    edit controller login

--- Impal_Restoran/application/controllers/Clogin.php
+++ Impal_Restoran/application/controllers/Clogin.php
@@ -7,13 +7,34 @@ class Clogin extends CI_Controller {
         $this->load->helper('url');
         $this->load->helper(array('form','url')); 
         $this->load->library('session');
+        $this->load->model('m_Karyawan');
     }
     public function index(){
         $this->load->view('index.php');
     }
 
     function Login(){
-        $this->load->view('index.php');
+        $id_karyawan = $this->input->post['id_karyawan'];
+        $password = $this->input->post['password'];
+        $result=$this->m_karyawan->login($id_karyawan,$password);
+        if($result['exist']>0){
+            $datasession = array(
+                'id_karyawan'  => $id_karyawan,
+                'password'     => $password,
+                'logged_in' => TRUE
+        );
+        
+        $this->session->set_userdata($datasession);
+        if ($this->session->datasession('id_karyawan')= "Ks"%){
+            $this->load->view('homekasir',$id_karyawan);
+        } else if(($this->session->datasession('id_karyawan')= "Pl"%)) {
+            $this->load->view('');
+        } else if(($this->session->datasession('id_karyawan')= "Sp"%)){
+            $this->load->view('');
+        }
+        }
+        
     }
    
+    }
 }
